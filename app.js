d2db5fa30e31c707b11946b920cdcca4466c0991
AjeetKu143 <ajeetkumar17013@gmail.com>
2025-03-19 14:35:14 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing");
+const ejsMate = require("ejs-mate");
+
+const port = 8080;
+const path = require("path");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate)
+app.use(express.static(path.join(__dirname, "/public")));
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/SwiftStay";
+
+main()
+  .then(() => {
+    console.log("Connection Successful!");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Index Route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings })
+})
+
+// New Route
+app.get("/listings/new", async (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+// show Route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+  // console.log(listing.image.url);
+
+});
+
+// Create Route
+app.post("/listings", async (req, res) => {
+  try {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Edit route 
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+
+// Update route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+// Delete route
+
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect(`/listings`);
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Something went wrong! Error: " + err.message);
+});
+
+app.get("/", (req, res) => {
+  res.send("welcome to SwiftStay");
+});
+
+app.listen(port, () => {
+  console.log(`listening on ${port}!`);
+});
